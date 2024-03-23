071ff0dcf68c96de9fa01df51653f59ecfffa6b4
Oduai aburub <154739571+OduaiAbrb@users.noreply.github.com>
2024-03-23 05:52:22 +0300

    Update DataPrepKit.py

--- DataPrepKit.py
+++ DataPrepKit.py
@@ -1,75 +1,185 @@
 import pandas as pd
-import numpy as np
-from DataPrepKit.missing_values import drop_missing_values, fill_missing_values
-from DataPrepKit.categorical_encoding import encode_categorical
-from DataPrepKit.outliers import detect_outliers
-from DataPrepKit.normalization import normalize_data
 
-def test_drop_missing_values():
-    data = pd.DataFrame({
-        'A': [1, 2, np.nan, 4],
-        'B': [5, np.nan, 7, 8],
-        'C': ['a', 'b', 'c', 'd']
-    })
-    expected_output = pd.DataFrame({
-        'A': [1.0, 2.0, 4.0],
-        'B': [5.0, 7.0, 8.0],
-        'C': ['a', 'b', 'd']
-    }, index=[0, 1, 3])
-    assert drop_missing_values(data).equals(expected_output)
 
-def test_fill_missing_values():
-    data = pd.DataFrame({
-        'A': [1, 2, np.nan, 4],
-        'B': [5, np.nan, 7, 8],
-        'C': ['a', 'b', 'c', 'd']
-    })
-    expected_output = pd.DataFrame({
-        'A': [1.0, 2.0, 3.0, 4.0],
-        'B': [5.0, 6.0, 7.0, 8.0],
-        'C': ['a', 'b', 'c', 'd']
-    })
-    assert fill_missing_values(data, method='mean').equals(expected_output)
+class DataPrepKit:
+    
+    def read_file(self, file_name):
+        fname_split = file_name.split('.')
+        df = None
+        
+        if fname_split[-1] == 'csv':
+            df = pd.read_csv(file_name)
+        
+        elif fname_split[-1] == 'xlsx':
+            df = pd.read_excel(file_name)
+        
+        elif fname_split[-1] == 'json':
+            df = pd.read_json(file_name)
+            df = df.replace('', None)
+        
+        return df
+    
+    
+    def __get_data_type(self, df_column):
+        dtype = ''
 
-def test_encode_categorical():
-    data = pd.DataFrame({
-        'A': [1, 2, 3, 4],
-        'B': ['a', 'b', 'a', 'b'],
-        'C': ['x', 'y', 'x', 'y']
-    })
-    expected_output = pd.DataFrame({
-        'A': [1, 2, 3, 4],
-        'B_a': [1, 0, 1, 0],
-        'B_b': [0, 1, 0, 1],
-        'C': ['x', 'y', 'x', 'y']
-    })
-    assert encode_categorical(data, column='B', method='one-hot').equals(expected_output)
+        if str(type(df_column.describe().dtype)) == "<class 'numpy.dtype[object_]'>":
+            dtype = 'string'
+        elif str(type(df_column.sum())) == "<class 'numpy.int64'>":
+            dtype = 'int'
+        elif str(type(df_column.sum())) == "<class 'numpy.float64'>":
+            dtype = 'float'
 
-def test_detect_outliers():
-    data = pd.DataFrame({
-        'A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100],
-        'B': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1000]
-    })
-    expected_output = pd.DataFrame({
-        'A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        'B': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    })
-    assert detect_outliers(data, method='zscore', threshold=3).equals(expected_output)
+        return dtype
+    
+    
+    def __most_frequent(self, df_column):
+        unique = {}
+        freq, top = 0, ''
 
-def test_normalize_data():
-    data = pd.DataFrame({
-        'A': [1, 2, 3, 4, 5],
-        'B': [10, 20, 30, 40, 50]
-    })
-    expected_output = pd.DataFrame({
-        'A': [0.0, 0.25, 0.5, 0.75, 1.0],
-        'B': [0.0, 0.25, 0.5, 0.75, 1.0]
-    })
-    assert normalize_data(data).equals(expected_output)
+        for item in reversed(df_column):
+            unique[item] = unique.get(item, 0) + 1
+            if unique[item] >= freq :
+                freq, top = unique[item], item
 
-if __name__ == "__main__":
-  test_drop_missing_values()
-  test_fill_missing_values()
-  test_encode_categorical()
-  test_detect_outliers()
-  test_normalize_data()
+        return unique, top, freq
+    
+    
+    def data_summary(self, df):
+        df_null = df.isnull().sum()
+        total_data = len(df)
+        
+        all_df = pd.DataFrame({'':['count', 'missing val', 'missing %', 'unique', 'most freq', 'most freq cnt', 'mean', 'data type']}).set_index('')
+
+        for col in df.columns:
+            data = []
+            df_describe = df[col].describe()
+            unique, top, freq = self.__most_frequent(df[col])
+
+            data.append(df_describe['count'])
+            data.append(df_null[col])
+            data.append('{:0.2f}%'.format((df_null[col]/total_data)*100))
+            data.append(len(unique))
+            data.append(top)
+            data.append(freq)
+            data.append('NaN' if self.__get_data_type(df[col]) == 'string' else df_describe['mean'])
+            data.append(self.__get_data_type(df[col]))
+
+            all_df.insert(loc=len(all_df.columns), column=col, value=data)
+            
+        pd.set_option('display.max_columns', None)
+        
+        return all_df
+    
+    
+    def row_missing_data_summary(self, df):
+        row_missing_count = df.isnull().sum(axis=1)
+        total_col = len(df.axes[1])
+        category = {
+            '0%'          : [0],
+            '1%  -> 25%'  : [0],
+            '25% -> 50%'  : [0],
+            '50% -> 75%'  : [0],
+            '75% -> 100%' : [0]
+        }
+        
+        for row in row_missing_count:
+            percentage = (row/total_col) * 100
+            
+            if percentage == 0:
+                category['0%'][0] += 1
+            elif percentage >= 1 and percentage < 25:
+                category['1%  -> 25%'][0] += 1
+            elif percentage >= 25 and percentage < 50:
+                category['25% -> 50%'][0] += 1
+            elif percentage >= 50 and percentage < 75:
+                category['50% -> 75%'][0] += 1
+            elif percentage >= 75 and percentage <= 100:
+                category['75% -> 100%'][0] += 1
+        
+        return pd.DataFrame(category)
+    
+    
+    def drop_columns(self, df, columns_list):
+        return df.drop(columns_list, axis = 1)
+    
+    
+    def drop_col_contains_missing_percentage(self, df, percentage):
+        keep_values = (1 - percentage/100) * len(df)
+        return df.dropna(thresh=keep_values, axis=1)
+    
+    
+    def drop_row_contains_missing_percentage(self, df, percentage):
+        keep_values = (1 - percentage/100) * len(df.axes[1])
+        return df.dropna(thresh=keep_values)
+    
+    
+    def replace_string(self, df_column):
+        unique_list = df_column.unique()
+        df_column_copy = df_column.copy()
+        
+        cnt = 0
+        for unique in unique_list:
+            if not pd.isnull(unique):
+                df_column_copy = df_column_copy.replace(to_replace = unique, value = cnt)
+                cnt += 1
+            
+        return df_column_copy
+    
+    
+    def __get_all_col_strings(self, df):
+        col_list = []
+        
+        for col in df.columns:
+            if self.__get_data_type(df[col]) == 'string':
+                col_list.append(col)
+                
+        return col_list
+    
+    
+    def replace_all_strings(self, df):
+        col_list = self.__get_all_col_strings(df)
+        df_copy = df.copy()
+        
+        for col in col_list:
+            df_copy[col] = self.replace_string(df_copy[col])
+
+        return df_copy
+    
+    
+    def mean_imputation(self, df_column):
+        df_column_copy = pd.Series(df_column)
+        mean_value = df_column_copy.mean()
+        
+        return df_column_copy.fillna(mean_value)
+    
+    
+    def median_imputation(self, df_column):
+        df_column_copy = pd.Series(df_column)
+        median_value = df_column_copy.median()
+        
+        return df_column_copy.fillna(median_value)
+    
+    
+    def mode_imputation(self, df_column):
+        df_column_copy = pd.Series(df_column)
+        _, mode_value, _ = self.__most_frequent(df_column_copy)
+        
+        return df_column_copy.fillna(mode_value)
+    
+    
+    def forward_fill_imputation(self, df_column):
+        df_column_copy = df_column.copy()
+        
+        df_column_copy = df_column_copy.ffill()
+
+        return df_column_copy
+    
+    
+    def backward_fill_imputation(self, df_column):
+        df_column_copy = df_column.copy()
+        
+        df_column_copy = df_column_copy.bfill()
+
+        return df_column_copy
+    
